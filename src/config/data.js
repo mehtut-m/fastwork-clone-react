b12a0ad0519b3f612a37d2d8cb525576ddc1a6d1
mehtut-m <mehtut.m@gmail.com>
2022-02-18 11:54:27 +0700

    added auth and user context

---
+++ src/config/data.js
@@ -0,0 +1,11 @@
+export const ACCESS_TOKEN = 'fast_work_access_token';
+
+// AUTH REDUCER
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'LOGIN_FAIL';
+export const LOGOUT = 'LOGOUT';
+export const LOADING = 'LOADING';
+export const AUTH_SUCCESS = 'AUTH_SUCCESS';
+export const AUTH_FAIL = 'AUTH_FAIL';
---
+++ src/reducers/user.js
@@ -0,0 +1,30 @@
+import {
+  REGISTER_SUCCESS,
+  LOGIN_SUCCESS,
+  AUTH_SUCCESS,
+  LOADING,
+  LOGOUT,
+} from '../config/data';
+
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
+    case AUTH_SUCCESS: {
+      console.log('User Reducer here');
+      console.log(action.payload);
+      return { ...state, ...action.payload };
+    }
+    case LOADING: {
+      return state;
+    }
+    case LOGOUT: {
+      return state;
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export default userReducer;
