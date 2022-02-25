896d237b8325dd16544a2ab4643fdc8e9ab3c6f3
same <sittichok.bo55@sau.ac.th>
2022-02-25 19:11:25 +0700

    initial commit

--- src/components/Home/FooterMap.jsx
+++ src/components/Home/FooterMap.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
-import { Box, List, ListItem, Typography } from '@mui/material';
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import PhoneIcon from '@mui/icons-material/Phone';
+import { List, ListItem, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
 function FooterMap({ item }) {
@@ -69,7 +62,8 @@ function FooterMap({ item }) {
 
         <Link to="#">
           <ListItem>{item.items15}</ListItem>
-          <Link to="#"></Link>
+        </Link>
+        <Link to="#">
           <ListItem>{item.items16}</ListItem>
         </Link>
       </List>
@@ -79,9 +73,17 @@ function FooterMap({ item }) {
 
         <Link to="#">
           <ListItem>{item.items17}</ListItem>
+        </Link>
+        <Link to="#">
           <ListItem>{item.items18}</ListItem>
+        </Link>
+        <Link to="#">
           <ListItem>{item.items19}</ListItem>
+        </Link>
+        <Link to="#">
           <ListItem>{item.items20}</ListItem>
+        </Link>
+        <Link to="#">
           <ListItem>{item.items21}</ListItem>
         </Link>
       </List>
